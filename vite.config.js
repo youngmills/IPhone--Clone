dd710768d57828df88902768de283240caac8b66
youngmills <iangordon047@gmail.com>
2025-01-20 21:01:02 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "university-of-nairobi-b3",
-    project: "javascript-react"
+    project: "javascript-react",
+    base: '/IPhone--Clone/'
   })],
 
   build: {
