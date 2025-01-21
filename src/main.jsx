413eca534431633ed8524bc93792680e899e686d
youngmills <iangordon047@gmail.com>
2025-01-21 09:18:51 +0300

    Set base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -28,4 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <BrowserRouter basename="/IPhone--Clone/">
+    <App />
+  </BrowserRouter>,
 )
