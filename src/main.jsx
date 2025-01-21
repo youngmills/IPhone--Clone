b437a62972386e1427d640fae580b5e3e9c14e4e
youngmills <iangordon047@gmail.com>
2025-01-21 11:17:47 +0300

    Set base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,7 +6,7 @@ import './index.css'
 //...
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
+/*Sentry.init({
   dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
   integrations: [
     Sentry.browserTracingIntegration(),
@@ -23,13 +22,11 @@ Sentry.init({
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0, 
-});
+});*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  <BrowserRouter basename="/IPhone--Clone/">
-    <App />
-  </BrowserRouter>,
+  
 )
