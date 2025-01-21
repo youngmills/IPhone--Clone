5fed493b14f652c542670dd315d738f62138c9c0
youngmills <iangordon047@gmail.com>
2025-01-21 18:10:03 +0300

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [react(), sentryVitePlugin({
-    org: "university-of-nairobi-b3",
-    project: "javascript-react",
-    telemetry: false,
-  })],
+  base: '/IPhone--Clone/',
+  plugins: [react(), 
+
+  ],
 
   build: {
     sourcemap: true
